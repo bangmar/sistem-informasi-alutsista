c5c5d5e8542ce426382d048782c3c146b557a89d
MARIO SIHALOHO <20102018@ittelkom-pwt.ac.id>
2023-10-04 00:16:55 +0700

    feature:responsive footer

--- src/components/layouts/base-layout/index.tsx
+++ src/components/layouts/base-layout/index.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/molecules/footer";
 import Navbar from "@/components/molecules/navbar";
 import { FC, Fragment, ReactNode } from "react";
 
@@ -10,6 +11,7 @@ const Baselayout: FC<TBaseLayoutProps> = ({ children }) => {
 		<Fragment>
 			<Navbar />
 			<main className='px-28 min-h-[100vh]'>{children}</main>
+			<Footer />
 		</Fragment>
 	);
 };
